466e9155dab930da10dee5d8cec8cd936fafd121
liese <liese@f19feb9e-661e-4a74-b561-ff9f2d4e076c>
2014-11-06 22:34:27 +0000

    Server/Firmware: implemented efficient canonical Huffman encoding

--- app/ejb/src/java/com/myfridget/server/util/HuffmanCompression.java
+++ app/ejb/src/java/com/myfridget/server/util/HuffmanCompression.java
@@ -7,6 +7,7 @@ package com.myfridget.server.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,14 +24,27 @@ public class HuffmanCompression
     {
         public final int frequency; // the frequency of this tree
         public final short symbol;
-        public final int depth; // current depth at this node level
         public final HuffmanTree left, right; // subtrees
-        public HuffmanTree(int freq, short sym) { frequency = freq; symbol = sym; left = null; right = null; depth = 0;}
-        public HuffmanTree(HuffmanTree l, HuffmanTree r) { frequency = l.frequency + r.frequency; left = l; right = r; symbol = 0; depth = Math.max(l.depth, r.depth) + 1;}
+        public HuffmanTree(int freq, short sym) { frequency = freq; symbol = sym; left = null; right = null;}
+        public HuffmanTree(HuffmanTree l, HuffmanTree r) { frequency = l.frequency + r.frequency; left = l; right = r; symbol = 0;}
 
         @Override
         public int compareTo(HuffmanTree o) {
-            return frequency - o.frequency == 0 ? symbol - o.symbol : frequency - o.frequency;
+            return frequency - o.frequency;
+        }
+    }
+    
+    public static class DictionaryEntry implements Comparable<DictionaryEntry>{
+        public final int length;
+        public final short symbol;
+        public String code = null;
+        public DictionaryEntry(int len, short sym) {length=len; symbol=sym;}
+
+        @Override
+        public int compareTo(DictionaryEntry o) {
+            // sort order for canonical huffman: 
+            // see http://en.wikipedia.org/wiki/Canonical_Huffman_code
+            return length == o.length ? symbol - o.symbol : length - o.length;
         }
     }
     
@@ -46,70 +60,94 @@ public class HuffmanCompression
             HuffmanTree a = trees.poll();
             HuffmanTree b = trees.poll();
 
-            // put into new node and re-insert into queue, shorter depths go left
-            trees.offer(a.depth <= b.depth ? new HuffmanTree(a, b) : new HuffmanTree(b, a));
+            // put into new node and re-insert into queue
+            trees.offer(new HuffmanTree(a, b));
         }
         return trees.poll();
     }
     
-    public static int buildDictionary(HuffmanTree tree, StringBuffer prefix, Map<Short,String> dictionary, List<Short> lrIterator) {
+    protected static void buildDictionaryTraverseTree(HuffmanTree tree, int prefixLength, List<DictionaryEntry> dictionary) {
         if (tree.left == null) {
-            dictionary.put(tree.symbol, prefix.toString());
-            lrIterator.add(tree.symbol);
-            return tree.frequency * prefix.length();
+            dictionary.add(new DictionaryEntry(prefixLength, tree.symbol));
         } else {
             // traverse left
-            prefix.append('0');
-            int sc1 = buildDictionary(tree.left, prefix, dictionary, lrIterator);
-            prefix.deleteCharAt(prefix.length()-1);
-
+            buildDictionaryTraverseTree(tree.left, prefixLength + 1, dictionary);
             // traverse right
-            prefix.append('1');
-            int sc2 = buildDictionary(tree.right, prefix, dictionary, lrIterator);
-            prefix.deleteCharAt(prefix.length()-1);
-            
-            return sc1 + sc2;
+            buildDictionaryTraverseTree(tree.right, prefixLength + 1, dictionary);
         }
     }
     
-    public static byte[] encodeDictionary(Map<Short,String> dictionary, List<Short> lrIterator) {
-        byte[] result = new byte[lrIterator.size()*2+1];
-        result[0] = (byte)lrIterator.size();
-        for (int i=0; i<lrIterator.size(); i++) {
-            short symbol = lrIterator.get(i);
-            result[1+(i*2)] = (byte)symbol;
-            result[1+(i*2)+1] = (byte)dictionary.get(symbol).length();
+    public static List<DictionaryEntry> buildDictionary(HuffmanTree tree) {
+        List<DictionaryEntry> dictionary = new ArrayList<>();
+        buildDictionaryTraverseTree(tree, 0, dictionary);
+        // now order by code length, symbol asc:
+        dictionary.sort(null);
+        // create canonical huffman code:
+        int currentLen = 0;
+        long prefix = 1;
+        for (DictionaryEntry entry : dictionary) {
+            prefix <<= entry.length-currentLen;
+            entry.code = Long.toString(prefix, 2).substring(1);
+            currentLen = entry.length;
+            prefix++;
         }
-        return result;
+        return dictionary;
     }
     
-    public static void printDictionary(Map<Short, String> dictionary, List<Short> lrIterator) {
+    public static Map<Short,String> buildDictionaryLookupMap(List<DictionaryEntry> dictionary) {
+        Map<Short,String> map = new HashMap<>();
+        dictionary.forEach(i->map.put(i.symbol, i.code));
+        return map;
+    }
+    
+    public static byte[] encodeDictionary(List<DictionaryEntry> dictionary) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(dictionary.size()*2);
+        // write out number of symbols (=dictionary size)
+        baos.write(dictionary.size());
+        int currentLen = 0;
+        int currentCount = 0;
+        // write out number of symbols of length 1,2,3...n (n=maximum prefix length)
+        for (DictionaryEntry entry : dictionary) {
+            if (entry.length > currentLen) {
+                if (currentCount>0) baos.write(currentCount);
+                for (int i=0; i<entry.length-currentLen-1; i++) baos.write(0);
+                currentLen = entry.length;
+                currentCount = 0;
+            }
+            currentCount++;
+        }
+        if (currentCount>0) baos.write(currentCount);
+        // write out list of symbols:
+        for (DictionaryEntry entry : dictionary) baos.write(entry.symbol);
+        try {baos.close();} catch (IOException e) {}
+        return baos.toByteArray();
+    }
+    
+    public static void printDictionary(List<DictionaryEntry> dictionary) {
         System.out.println("SYMBOL\tHUFFMAN CODE");
-        for (short symbol : lrIterator) {
-            // print out character, frequency, and code for this leaf (which is just the prefix)
-            System.out.println(symbol + "\t" + dictionary.get(symbol));
-        }
+        // print out character, frequency, and code for this leaf (which is just the prefix)
+        dictionary.forEach(i->System.out.println(i.symbol + "\t" + i.code));
     }
     
     public static byte[] compress(byte[] data) throws IOException {
         System.out.println("ORIG SIZE: " + data.length);
+
         int[] counter = new int[256];
         for (byte b : data) counter[b&0xff]++;
-        HuffmanTree tree = buildHuffmanTree(counter);
 
-        Map<Short, String> dictionary = new HashMap<>();
-        List<Short> lrIterator = new ArrayList<>();
-        int expectedResultSize = buildDictionary(tree, new StringBuffer(), dictionary, lrIterator);
+        HuffmanTree tree = buildHuffmanTree(counter);
+        List<DictionaryEntry> dictionary = buildDictionary(tree);
+        printDictionary(dictionary);
         
-        printDictionary(dictionary, lrIterator);
-        byte[] encodedDictionary = encodeDictionary(dictionary, lrIterator);
+        byte[] encodedDictionary = encodeDictionary(dictionary);
         
         // compress data
-        ByteArrayOutputStream baos = new ByteArrayOutputStream((int)Math.round(Math.ceil(((double)expectedResultSize)/8)));
+        Map<Short,String> map = buildDictionaryLookupMap(dictionary);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         baos.write(encodedDictionary);
         BitOutputStream compressOut = new BitOutputStream(baos);
         for (byte b : data) {
-            for (char bit : dictionary.get((short)(b&0xff)).toCharArray()) compressOut.write(bit=='1');
+            for (char bit : map.get((short)(b&0xff)).toCharArray()) compressOut.write(bit=='1');
         }
         compressOut.close();
         System.out.println("DICTIONARY SIZE: " + encodedDictionary.length);
